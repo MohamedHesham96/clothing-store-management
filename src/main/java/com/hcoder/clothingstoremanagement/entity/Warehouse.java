ed8265e10c1925dfb09a45fdf80ac51446e0943b
MohamedHesham96 <mohamed.hesham.fci@gmail/com>
2020-02-04 21:38:20 +0200

    Add Warehouse Entity

---
+++ src/main/java/com/hcoder/clothingstoremanagement/entity/Warehouse.java
@@ -0,0 +1,55 @@
+package com.hcoder.clothingstoremanagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Warehouse {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	@Column(name = "quantity")
+	int quantity;
+
+	@Column(name = "trade_price")
+	int trade_price;
+
+	@Column(name = "item")
+	String item;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getTrade_price() {
+		return trade_price;
+	}
+
+	public void setTrade_price(int trade_price) {
+		this.trade_price = trade_price;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+}
