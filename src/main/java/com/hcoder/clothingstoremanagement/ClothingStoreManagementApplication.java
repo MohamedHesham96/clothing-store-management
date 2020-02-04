c3ea6bf37347b15c0134489d2b7408c7101df0ae
MohamedHesham96 <mohamed.hesham.fci@gmail/com>
2020-02-04 18:22:39 +0200

    First Commit

---
+++ src/main/java/com/hcoder/clothingstoremanagement/ClothingStoreManagementApplication.java
@@ -0,0 +1,12 @@
+package com.hcoder.clothingstoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingStoreManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hcoder/clothingstoremanagement/ClothingStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcoder.clothingstoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
